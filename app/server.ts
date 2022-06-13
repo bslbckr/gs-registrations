7296d0ea888aee4601ec287322f01ddc3a467b46
bslbckr <basil@ultical.com>
2022-06-14 01:06:37 +0200

    Add a request logger

--- app/server.ts
+++ app/server.ts
@@ -3,13 +3,22 @@ import { registrationRouter } from './router/registration';
 import { init } from './utils/mysql.connector';
 import * as path from 'path';
 import multer from 'multer';
+import morgan from 'morgan';
 
 const server: express.Application = express();
 const upload = multer();
 
 init();
+server.use(morgan('combined'));
 server.use(express.urlencoded({ extended: false }));
 
 server.use('/', upload.none(), registrationRouter);
 server.use(express.static(path.join(__dirname, '../public')));
-server.listen(8080);
+
+if (server.get('env') === 'production') {
+    console.info("running in production, listening on 0.0.0.0:8080");
+    server.listen(8080, "0.0.0.0");
+} else {
+    console.info("running in dev, lisening on localhost:8080");
+    server.listen(8080);
+}
