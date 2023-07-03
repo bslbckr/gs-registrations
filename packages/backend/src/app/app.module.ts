0dbefca2fa19604d727c14df7fadedbb17d75bdc
bslbckr <basil@ultical.com>
2023-07-03 23:45:44 +0200

    Change provided classes

--- packages/backend/src/app/app.module.ts
+++ packages/backend/src/app/app.module.ts
@@ -1,11 +1,11 @@
-import { APP_INITIALIZER, NgModule } from '@angular/core';
+import { APP_INITIALIZER, inject, NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { RegistrationService } from './registration.service';
 import { RegistrationsComponent } from './registrations/registrations.component';
-import { AuthConfig, OAuthModule } from 'angular-oauth2-oidc';
+import { AuthConfig, OAuthModule, OAuthModuleConfig, OAuthService, OAuthStorage } from 'angular-oauth2-oidc';
 import { StartComponent } from './start/start.component';
 import { StatehandlerProcessorService, StatehandlerProcessorServiceImpl } from './services/statehandler-processor.service';
 import { environment } from 'src/environments/environment';
@@ -14,11 +14,13 @@ import { PlatformLocation } from '@angular/common';
 import { MatTableModule } from '@angular/material/table';
 import { MatCheckboxModule } from '@angular/material/checkbox';
 import { HttpClientModule } from '@angular/common/http';
+import { Router } from '@angular/router';
+import { AuthService } from './services/auth.service';
+
 const authConfig: AuthConfig = {
     issuer: environment.oidc.issuer,
     clientId: environment.oidc.clientId,
     responseType: 'code',
-    //    redirectUri: window.location.origin  + '/auth/callback',
     scope: 'openid profile email offline_access manager',
     oidc: true,
     strictDiscoveryDocumentValidation: false,
@@ -26,11 +28,7 @@ const authConfig: AuthConfig = {
 
 };
 
-const stateHandlerFn = (stateHandler: StatehandlerService) => {
-    return () => {
-        return stateHandler.initStateHandler();
-    };
-};
+function storageFactory(): OAuthStorage { return localStorage; }
 
 @NgModule({
     declarations: [
@@ -41,12 +39,7 @@ const stateHandlerFn = (stateHandler: StatehandlerService) => {
     imports: [
         BrowserModule,
         HttpClientModule,
-        OAuthModule.forRoot({
-            resourceServer: {
-                allowedUrls: ['/api/backend'],
-                sendAccessToken: true
-            }
-        }),
+        OAuthModule.forRoot(),
         AppRoutingModule,
         MatTableModule,
         MatCheckboxModule
@@ -55,15 +48,44 @@ const stateHandlerFn = (stateHandler: StatehandlerService) => {
         { provide: RegistrationService },
         {
             provide: AuthConfig,
-            useFactory: (s: PlatformLocation) => {
-                authConfig.redirectUri = window.location.origin + s.getBaseHrefFromDOM() + 'auth/callback';
+            useFactory: () => {
+                authConfig.redirectUri = window.location.origin + inject(PlatformLocation).getBaseHrefFromDOM() + 'auth/callback';
                 return authConfig;
-            },
-            deps: [PlatformLocation]
+            }
         },
+        {
+            provide: OAuthStorage,
+            useFactory: storageFactory
+        },
+        {
+            provide: OAuthModuleConfig,
+            useValue: {
+                resourceServer: {
+                    allowedUrls: ['/api/backend'],
+                    sendAccessToken: true
+                }
+            }
+        },
+        { provide: AuthService },
         { provide: StatehandlerProcessorService, useClass: StatehandlerProcessorServiceImpl },
-        { provide: StatehandlerService, useClass: StatehandlerServiceImpl },
-        { provide: APP_INITIALIZER, useFactory: stateHandlerFn, multi: true, deps: [StatehandlerService] }
+        {
+            provide: StatehandlerService,
+            useFactory: () => {
+                const oauth = inject(OAuthService);
+                const proc = inject(StatehandlerProcessorService);
+                return new StatehandlerServiceImpl(oauth, proc);
+            }
+        },
+        {
+            provide: APP_INITIALIZER,
+            multi: true,
+            useFactory: () => {
+                const rtr = inject(Router);
+                const handler = inject(StatehandlerService);
+
+                return () => handler.initStateHandler(rtr);
+            }
+        }
     ],
     bootstrap: [AppComponent]
 })
